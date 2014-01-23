jcfr/CDash-svn-mirror

commit 41ff6a40ffad76aac516e8fe89346ec2c08d4201

ENH: Only keeping feed for 30 days
git-svn-id: https://www.kitware.com/svn/CDash/trunk@3432 91079597-3006-4846-9bd3-4f3fb3d139a7

models/feed.php
@@ -295,7 +295,15 @@ class Feed
     $this->Insert($projectid,$buildid,Feed::TypeUpdate,$description);
     }
 
-  /** Insert a new site */
+  /** Delete the old feeds */
+  function DeleteOld($projectid,$days)
+    {
+    $secondsinday = 86400; // == 3600*24;
+    $olddate_utc = gmdate(FMT_DATETIMESTD, time()-$days*$secondsinday);
+    pdo_delete_query("DELETE FROM feed WHERE projectid=".$projectid." AND date<".$olddate_utc);
+    } // end DeleteOld()
+
+  /** Insert a new feed */
   function Insert($projectid,$buildid,$type,$description="")
     {
     $this->ProjectId = $projectid;
@@ -314,6 +322,9 @@ class Feed
       add_last_sql_error("Feed Insert");
       return false;
       }
+
+    // Delete the old feed (30 days)
+    $this->DeleteOld($projectid,30);
 
     } // end function insert
 

commit 6a38e3c34332b0cc5e6e82282ca5fb2ba6831681

BUG: 14717 Fix SQL query quoting issue introduced in r3432
git-svn-id: https://www.kitware.com/svn/CDash/trunk@3433 91079597-3006-4846-9bd3-4f3fb3d139a7

models/feed.php
@@ -300,7 +300,7 @@ class Feed
     {
     $secondsinday = 86400; // == 3600*24;
     $olddate_utc = gmdate(FMT_DATETIMESTD, time()-$days*$secondsinday);
-    pdo_delete_query("DELETE FROM feed WHERE projectid=".$projectid." AND date<".$olddate_utc);
+    pdo_delete_query("DELETE FROM feed WHERE projectid='$projectid' AND date<'$olddate_utc'");
     } // end DeleteOld()
 
   /** Insert a new feed */