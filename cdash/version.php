6cb84e9e13413963bbf21446dccef70d8d2c13e5
Zack Galbreath <zack.galbreath@kitware.com>
2014-05-30 13:42:20 +0000

    initial import

---
+++ cdash/version.php
@@ -0,0 +1,23 @@
+<?php
+/*=========================================================================
+
+  Program:   CDash - Cross-Platform Dashboard System
+  Module:    $Id: version.php 3479 2014-05-09 07:01:09Z jjomier $
+  Language:  PHP
+  Date:      $Date: 2014-05-09 07:01:09 +0000 (Fri, 09 May 2014) $
+  Version:   $Revision: 3479 $
+
+  Copyright (c) 2002 Kitware, Inc.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+// Current version
+$CDASH_VERSION_MAJOR = '2';
+$CDASH_VERSION_MINOR = '3';
+$CDASH_VERSION_PATCH = '0';
+$CDASH_VERSION = $CDASH_VERSION_MAJOR.".".$CDASH_VERSION_MINOR.".".$CDASH_VERSION_PATCH;
+?>
